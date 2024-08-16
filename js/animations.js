75e18c13d37d5648215e123eed19eec9d7ea3168
micha <chatzakisdev@gmail.com>
2024-08-16 18:09:37 +0300

    Addition of the "Gym Rat Notes Web project".

--- js/animations.js
+++ js/animations.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if( bottom_of_window > bottom_of_object - 50){
+            if( bottom_of_window > bottom_of_object - 80){
                 $(this).addClass('animate');
             }
         }); 
